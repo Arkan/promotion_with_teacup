3a49120c3fd53ae68fb7a6ebb81993e1cc71d12c
Arkan <florian.bertholin@gmail.com>
2013-05-19 18:14:16 +0200

    Simplify example

--- app/styles/appearence.rb
+++ app/styles/appearence.rb
@@ -1,27 +1,16 @@
 Teacup::Appearance.new do
 
   style UINavigationBar, {
-    tintColor: UIColor.blackColor
+    tintColor: UIColor.blueColor
   }
 
   style UIBarButtonItem, {
     # when_contained_in: UINavigationBar,
-    tintColor: UIColor.blackColor
-  }
-
-  style UITableViewCell, {
-    layer: {  # style the layer!
-      shadowRadius: 3
-    },
-    backgroundView: {  # style the background!
-      backgroundColor: UIColor.blackColor
-    },
-    imageView: {  # style the imageView!
-      contentMode: UIViewContentModeScaleAspectFill
-    }
+    tintColor: UIColor.yellowColor
   }
 
   style UISearchBar, {
-    tintColor: UIColor.colorWithRed(0.733, green:0.733, blue:0.733, alpha:1)
+    tintColor: UIColor.redColor
   }
+
 end
